5f5b7493e3885e26e754bc54e4023b9fd56febac
junuleedk <junulee.dk@gmail.com>
2024-01-10 17:54:42 +0900

    2024 01 10까지

---
+++ src/ex12inheritance/Test5.java
@@ -0,0 +1,12 @@
+package ex12inheritance;
+
+class Person extends Object {
+	String name;
+}
+public class Test5 {
+  @Override
+public String toString() {
+	// TODO Auto-generated method stub
+	return super.toString();
+}
+}
---
+++ src/ex13interface/Test1.java
@@ -0,0 +1,30 @@
+package ex13interface;
+
+abstract class Car {
+	abstract void absFunc();
+}
+
+ class SportCar extends Car {
+	 @Override
+	void absFunc() {
+	}
+}
+
+ class SUV extends Car {
+	 @Override
+	void absFunc() {
+	}
+ }
+
+public class Test1 {
+	
+	public static void mai(String[] args) {
+//		Car car = new Car();
+		SportCar sportCar = new SportCar();
+		SUV suv = new SUV();
+		
+		Car arr[] = new Car[10];
+		arr[0] = sportCar;
+		arr[1] = suv;
+	}
+}
