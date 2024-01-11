64c36b18b703b68e9cd661efd6f73d6beb48b23b
junuleedk <junulee.dk@gmail.com>
2024-01-11 17:17:29 +0900

    2024년 01월 11일까지

---
+++ src/ex14innerclass/test1.java
@@ -0,0 +1,28 @@
+package ex14innerclass;
+
+class A {
+	void myFunc() {
+		System.out.println("나는 A입니다.");
+	}
+}
+
+class B extends A {
+	@Override
+	void myFunc() {
+		System.out.println("나는 B처럼 살거야");
+	}
+}
+public class test1 {
+
+	public static void main(String[] args) {
+		A a = new A() {
+			@Override
+			void myFunc() {
+				System.out.println("나는 익명클래스");
+			}
+		};
+		a.myFunc();
+	}
+	
+
+}
